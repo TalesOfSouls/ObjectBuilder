f9ee8f5b1e38e1af43743d95902542d748b3d980
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-09-24 00:43:37 +0200

    1. added debug/release options for build. 2. added byte reading options. 3. started fixing png, probably still not working

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 cls
+set "EXE_NAME=obj_builder_win32"
+set "DESTINATION_DIR=..\build\objbuilder"
 
 IF NOT EXIST ..\build mkdir ..\build
-IF NOT EXIST ..\build\objbuilder mkdir ..\build\objbuilder
+IF NOT EXIST "%DESTINATION_DIR%" mkdir "%DESTINATION_DIR%"
 
 if not defined DevEnvDir (call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat")
 
@@ -10,20 +12,41 @@ if "%Platform%" neq "x64" (
     exit /b 1
 )
 
-cd ..\build\objbuilder
+cd "%DESTINATION_DIR%"
 del *.pdb > NUL 2> NUL
 del *.idb > NUL 2> NUL
 cd ..\..\ObjectBuilder
 
 REM Use /showIncludes for include debugging
 
+set BUILD_TYPE=DEBUG
+set BUILD_FLAGS=/Od /Oi /Z7 /WX /FC /DDEBUG
+
+set "DEBUG_DATA=/Fd"%DESTINATION_DIR%\%EXE_NAME%.pdb" /Fm"%DESTINATION_DIR%\%EXE_NAME%.map""
+
+REM Parse command-line arguments
+if "%1"=="-r" (
+    set BUILD_TYPE=RELEASE
+    set BUILD_FLAGS=/O2 /D NDEBUG
+
+    set DEBUG_DATA=
+)
+if "%1"=="-d" (
+    set BUILD_TYPE=DEBUG
+    set BUILD_FLAGS=/Od /Oi /Z7 /WX /FC /DDEBUG
+
+    set "DEBUG_DATA=/Fd"%DESTINATION_DIR%\%EXE_NAME%.pdb" /Fm"%DESTINATION_DIR%\%EXE_NAME%.map""
+)
+
 REM Create main program
 cl ^
-    /MT /nologo /Gm- /GR- /EHsc /Od /Oi /WX /W4 /FC /Z7 /wd4201 /wd4100 ^
+    %BUILD_FLAGS% /MT /nologo /Gm- /GR- /EHsc /W4 /wd4201 /wd4706 ^
+    /wd4018 /wd4389 /wd4100 /wd4244 /wd4189 ^
     /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /std:c++20 ^
-    /D WIN32 /D _WINDOWS /D _UNICODE /D UNICODE /D _CRT_SECURE_NO_WARNINGS ^
-    /Fo"../build/objbuilder/" /Fe"../build/objbuilder/obj_builder.exe" /Fd"../build/objbuilder/obj_builder.pdb" /Fm"../build/objbuilder/obj_builder.map" ^
-    "obj_builder.cpp"^
+    /D WIN32 /D _WINDOWS /D _UNICODE /D UNICODE ^
+    /D _CRT_SECURE_NO_WARNINGS ^
+    /Fo"%DESTINATION_DIR%/" /Fe"%DESTINATION_DIR%/%EXE_NAME%.exe" %DEBUG_DATA% ^
+    "%EXE_NAME%.cpp" ^
     /link /INCREMENTAL:no ^
     /SUBSYSTEM:CONSOLE /MACHINE:X64 ^
     kernel32.lib user32.lib gdi32.lib winmm.lib
